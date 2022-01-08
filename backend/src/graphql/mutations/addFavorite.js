49b122b27d5bcfde1bf1c2e86e2cb163a26383d0
Ville <ville.2000.vhk@gmail.com>
2022-01-08 22:12:36 +0200

    cleaning + tab fix

--- backend/src/graphql/mutations/addFavorite.js
+++ backend/src/graphql/mutations/addFavorite.js
@@ -1,22 +1,20 @@
 import { gql, UserInputError } from "apollo-server";
-import User from "../../models/user.js";
-import bcrypt from 'bcrypt';
-import * as yup from 'yup';
-import jwt from 'jsonwebtoken';
+import * as yup from "yup";
 import dotenv from "dotenv";
-import Driver from '../../models/driver'
-import Race from '../../models/race'
+import Driver from "../../models/driver";
+import Race from "../../models/race";
 dotenv.config();
 
 const typeDefs = gql`
-extend type Mutation {
-  addFavorite(driverID: String, raceID: String): User
-} `
+  extend type Mutation {
+    addFavorite(driverID: String, raceID: String): User
+  }
+`;
 
 const argsSchema = yup.object({
   driverID: yup.string().nullable(true),
-  raceID: yup.string().nullable(true)
-})
+  raceID: yup.string().nullable(true),
+});
 const resolvers = {
   Mutation: {
     addFavorite: async (root, args, context) => {
@@ -27,29 +25,33 @@ const resolvers = {
       }
       if (driverID) {
         const driver = await Driver.findById(driverID);
-        if (!driver)
-          throw new UserInputError("No driver found with that id");
-        if (currentUser.favorites.drivers.some(d => d.id === driver.id))
-          currentUser.favorites.drivers = currentUser.favorites.drivers.filter(favoriteDriver => favoriteDriver.id !== driver.id );
+        if (!driver) throw new UserInputError("No driver found with that id");
+        if (currentUser.favorites.drivers.some((d) => d.id === driver.id))
+          currentUser.favorites.drivers = currentUser.favorites.drivers.filter(
+            (favoriteDriver) => favoriteDriver.id !== driver.id
+          );
         else
-          currentUser.favorites.drivers = currentUser.favorites.drivers.concat(driver);
+          currentUser.favorites.drivers =
+            currentUser.favorites.drivers.concat(driver);
 
         return await currentUser.save();
       }
       if (raceID) {
-        const race = await Race.findById(raceID)
-        if (!race)
-          throw new UserInputError("No race found with that id");
+        const race = await Race.findById(raceID);
+        if (!race) throw new UserInputError("No race found with that id");
 
-        if (currentUser.favorites.races.some(r => r.id === race.id))
-          currentUser.favorites.races = currentUser.favorites.races.filter(favoriteRace => favoriteRace.id !== race.id);
+        if (currentUser.favorites.races.some((r) => r.id === race.id))
+          currentUser.favorites.races = currentUser.favorites.races.filter(
+            (favoriteRace) => favoriteRace.id !== race.id
+          );
         else
-          currentUser.favorites.races = currentUser.favorites.races.concat(race);
+          currentUser.favorites.races =
+            currentUser.favorites.races.concat(race);
         return await currentUser.save();
       }
       return currentUser;
-    }
-  }
-}
+    },
+  },
+};
 
 export default { typeDefs, resolvers };
